44eb641af8a7b5f24cc1fd72344a9925a48486b2
Jerry Howard <jerryhoward@fastmail.fm>
2019-09-26 22:27:17 +0000

    Generates Translate channel with 'respond' action + requisite JS

---
+++ app/channels/translate_channel.rb
@@ -0,0 +1,12 @@
+class TranslateChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def respond
+  end
+end
